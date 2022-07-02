b62c3776401cc666e7333272cb25e991a92644ad
dhruv-203 <102210361+dhruv-203@users.noreply.github.com>
2022-07-02 15:25:23 +0530

    Delete C directory

--- C/scanf and printf function/area.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-// area of the rectangle using defualt values
-int main()
-{
-    int lenght = 35, breath = 65;
-    int area = lenght * breath;
-    printf("the area of the triangle is %d", area);
-    return 0;
-}
