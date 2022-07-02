1a029a06dc5883d11db8cb10591bf18fcef62bc3
dhruv-203 <102210361+dhruv-203@users.noreply.github.com>
2022-07-02 15:23:23 +0530

    Delete Python directory

--- Python/em2.py
+++
@@ -1,8 +0,0 @@
-import re
-reg=re.compile('[A]')
-word= input("Enter any word")
-z=reg.search(word)
-if z!=None:
-    print("'%s' contains 'A'" %(word))
-else:
-    print("'%s' doesn't contains A" %(word))
--- Python/pr3.py
+++
@@ -1,8 +0,0 @@
-class university:
-    def __init__(self,nam,yearofestd,cit):
-        self.name=nam
-        self.year_of_estd=yearofestd
-        self.city=cit
-
-class professor(university):
-    def __init__(self,desig,highest_qual,area_of_re,year_of_joi,year_of_expr,name_of_ins):
