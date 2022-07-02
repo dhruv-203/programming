3adb7898dc4ab27bf281500cf65fba39ca85af23
dhruv-203 <pateldhruv2460@gmail.com>
2022-07-02 16:55:10 +0530

    added some new files

---
+++ Second.java
@@ -0,0 +1,18 @@
+// java variables
+public class Second{
+    public static void main(String[] args){
+        String name ="Patel Dhruv"; 
+        int age= 18;
+        char single='s';
+        boolean isthis=true;
+        float pi=3.14F; // while mentioning the float vlues use F at the end to distinguish b/w the int and float
+        final String address="home"; // final keyword is like const keyword in c lang
+
+        System.out.print(name);
+        System.out.print(age);
+        System.out.print(single);
+        System.out.print(isthis);
+        System.out.print(pi);
+        System.out.print(address);
+    }
+}
