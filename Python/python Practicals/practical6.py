653f7e2ec59066b9c6c3c7b2daef93e51cf92cf4
dhruv-203 <102210361+dhruv-203@users.noreply.github.com>
2022-07-02 15:09:51 +0530

    Delete Python directory

--- Python/python Practicals/practical6.py
+++
@@ -1,8 +0,0 @@
-# B.Tech Computer Science and Engineering
-# Dhruv Patel
-# 202103103510445
-
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-
-print("Addition:", num1+num2,"\nSubtraction:", num1-num2,"\nMultiplication:", num1*num2, "\nDivision", num1/num2)
