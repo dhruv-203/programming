4b08d2890ea2886990e3f6f32d4c23dc2f785597
dhruv-203 <102210361+dhruv-203@users.noreply.github.com>
2022-07-02 15:20:37 +0530

    Add files via upload

---
+++ Practice/first.js
@@ -0,0 +1,29 @@
+marks={
+    dhruv:99,
+    ayush:50,
+    bk:41
+};
+console.log(marks.dhruv);
+console.log(marks.ayush);
+console.log(marks.bk);
+// alert(marks.dhruv)
+// console.log(prompt(marks.dhruv));
+arry=[[4,5,6],[1,2,30],[1,4,5,"dhruv"]];
+console.log((arry[2][2]));
+su="i am a string"
+arry.forEach(function(element) {
+    console.log(element)
+});
+console.log("this is loop ")
+for(var i=0;i<=arry[2].length;i++){
+    // console.log(i)
+    console.log(arry[2][i])
+}
+console.log("This is while loop")
+var i=0
+while(i<=arry[1].length){
+    console.log(arry[1][i]);
+    i++;
+}
+
+document.addEventListener()
