69472388ac1a4d6b13e4be07ddb37f52b2725457
dhruv-203 <102210361+dhruv-203@users.noreply.github.com>
2022-07-06 12:24:44 +0530

    Create third.php

---
+++ third.php
@@ -0,0 +1,16 @@
+<?php
+// various datatypes
+	$iamstring="This is a String Varibale";
+	$iamint=4578;
+	$iamfloat=45.65;
+	$iambooloean=true;
+  $iamarray=array(45,"forty-five",45.00);
+  $iamnull=null;
+  
+  var_dump($iamnull);
+  var_dump($iamarray);
+  var_dump($iamint);
+  var_dump($iambooloean);
+  var_dump($iamstring);
+  var_dump($iamfloat);
+?>
