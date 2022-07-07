ae77481e4944749fb8939bab78a870518cb932ff
dhruv-203 <102210361+dhruv-203@users.noreply.github.com>
2022-07-07 10:18:10 +0530

    Create float.php

---
+++ float.php
@@ -0,0 +1,12 @@
+<?php
+$num=45.6598;
+var_dump($num);
+
+echo "The largest possible floating point number: ", PHP_FLOAT_MAX, "\n";
+echo "The largest possible floating point number: ", PHP_FLOAT_MIN, "\n";
+echo "The Size of floating point number: ", PHP_FLOAT_SIZE, "\n";
+echo "the smallest Negative floating point: ", -PHP_FLOAT_MAX, "\n";
+echo "Value: ",PHP_FLOAT_DIG,"\n";
+echo "Value: ",PHP_FLOAT_EPSILON,"\n";
+
+?>
