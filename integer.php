140fd48b80f067cf42b82a433af94de7506aebd1
dhruv-203 <102210361+dhruv-203@users.noreply.github.com>
2022-07-07 10:01:38 +0530

    Create integer.php

---
+++ integer.php
@@ -0,0 +1,11 @@
+$num=45;
+$text="hello";
+echo "The Maximum value that can be supported in Int: ", PHP_INT_MAX,"\n";
+echo "The Minimum value that can be supported in Int: ", PHP_INT_MIN,"\n";
+echo "The Size of Int: ", PHP_INT_SIZE,"\n";
+
+// is_int();
+// is_integer();
+//is_long();
+var_dump(is_int($num));
+var_dump(is_int($text));
