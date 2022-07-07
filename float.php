830109bf1a0d1190accd81d23bed77b28a600567
dhruv-203 <102210361+dhruv-203@users.noreply.github.com>
2022-07-07 10:23:19 +0530

    Update float.php

--- float.php
+++ float.php
@@ -4,9 +4,15 @@ var_dump($num);
 
 echo "The largest possible floating point number: ", PHP_FLOAT_MAX, "\n";
 echo "The largest possible floating point number: ", PHP_FLOAT_MIN, "\n";
-echo "The Size of floating point number: ", PHP_FLOAT_SIZE, "\n";
 echo "the smallest Negative floating point: ", -PHP_FLOAT_MAX, "\n";
 echo "Value: ",PHP_FLOAT_DIG,"\n";
 echo "Value: ",PHP_FLOAT_EPSILON,"\n";
+
+
+// finite and infinite check
+$x=411e1234;
+var_dump($x);
+var_dump(is_finite($x));
+var_dump(is_infinite($x));
 
 ?>
