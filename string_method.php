88463f469d81475342b5265adb9847ac7d02841f
dhruv-203 <102210361+dhruv-203@users.noreply.github.com>
2022-07-07 09:53:05 +0530

    Create string_method.php

---
+++ string_method.php
@@ -0,0 +1,9 @@
+// string methods
+<?php
+$text="My Name is Dhruv Patel And i Am in 2 year";
+echo strlen($text)."\n";
+echo str_word_count($text)."\n";
+echo strrev($text)."\n";
+echo strpos($text, "Dhruv")."\n";
+echo str_replace("My", "your", $text)."\n";
+?>
