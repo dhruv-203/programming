5ffb0e38068247d806c12ddcbe5259e71be4699f
dhruv-203 <102210361+dhruv-203@users.noreply.github.com>
2022-07-07 10:43:45 +0530

    Update fourth.php

--- fourth.php
+++ fourth.php
@@ -1,19 +1,19 @@
-//error is not solved
+
 <?php
 //Class example
 define("pi",3.14);
 	class Volume{
-	  public function __construct($radius,$height){
+	  public function __construct($radius,$heigth){
 	    $this->radius=$radius;
-	    $this->$heigth=$heigth;
+	    $this->heigth=$heigth;
 	  }
 	  public function cal(){
-	    return "The Volume of the Cylinder is: " .$this->radius*$this->radius*$this->$height;
+	    return "The Volume of the Cylinder is: " .$this->radius*$this->radius*$this->heigth;
 	  }
 	}
 	$heigth= (float)readline("Enter the Height of the Cylinder");
 	$radius= (float)readline("Enter the radius of Cylinder");
-	$myobj= new Volume($radius,$height);
-	$myobj->cal();
-	
+	$myobj= new Volume($radius,$heigth);
+	echo $myobj->cal();
+  	
 ?>
