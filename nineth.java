aa249575fba4b8c5d04578f0805264fd575e9d52
dhruv-203 <pateldhruv2460@gmail.com>
2022-07-08 20:43:52 +0530

    added nineth file which takes the arguments in the terminal

---
+++ nineth.java
@@ -0,0 +1,9 @@
+// taking inputs from the terminal
+public class nineth{
+    public static void main(String[] args) {
+        int x= Integer.parseInt(args[0]);
+        int y=Integer.parseInt(args[1]);
+        System.out.print("The addition of the two numbers: ");
+        System.out.println(x+y);
+    }
+}
