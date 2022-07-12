209925b5dc9d9dae2aaf37bc8e11eaaf44835e62
dhruv-203 <pateldhruv2460@gmail.com>
2022-07-12 10:16:38 +0530

    minor changes in scope.java

--- scope.java
+++ scope.java
@@ -4,12 +4,12 @@ public class scope{
         String nam="dhruv";
         return nam;
     }
+    String name="i am instance variable";
     public static void main(String[] args) {
         scope obj = new scope();
-        String nam="i am instance variable";
         System.out.println(scope.nam);
         System.out.println(obj.method());
-        System.out.println(nam);
+        System.out.println(obj.name);
 
     }
 }
