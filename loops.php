9b65506cb4fa9e9abe030361868984abc6b6a45e
dhruv-203 <pateldhruv2460@gmail.com>
2022-07-18 11:01:42 +0530

    merged test.php with loops.php

--- loops.php
+++ loops.php
@@ -32,10 +32,38 @@ while($con<20){
     $con++;
 }
 foreach($number as $l){
-    echo $l."\n";
+    echo $l."  ";
 }
+echo "\n";
 
 // pascal's triangle 
+
+$line= (int) readline("Enter a Number: ");
+$icount=$line;
+$n=0; 
+while($icount>0){
+$jcount=0;
+$r=0;
+while($jcount<$icount){
+    echo " ";
+    $jcount++;
+}
+while($r<=$n){
+    echo ((factorial($n))/(factorial($r)*factorial($n-$r)))." ";
+    $r++;
+}
+echo "\n";
+$icount--;
+$n++;
+}
+function factorial($num){
+    if($num==1||$num==0){
+        return 1;
+    }
+    return $num*factorial($num-1);
+}
+
+
 
 
 
