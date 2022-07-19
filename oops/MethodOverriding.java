c024636a4e457d9f2a497b07301a46b2e97d6f5e
dhruv-203 <pateldhruv2460@gmail.com>
2022-07-19 09:25:59 +0530

    normal changes

--- oops/MethodOverriding.java
+++ oops/MethodOverriding.java
@@ -11,10 +11,13 @@ class A{
 }
 class B extends A{
     int a = 87; // this is attribute overriding 
+    // @Override //this will give error 
     void method(int A){
         System.out.println("a="+a);
         System.out.println(A+" value of A");
     }  // this is not overriding as parent class method doesn't have any parameter
+    @Override  // this makes sure that override is happening any changes due to which overriding disturbs it prevents from happening that and displays an error 
+    //The method method1() of type B must override or implement a supertype method eeror msg 
     void method1(){
         System.out.println("a+b="+34);
     }  // this is overriding
