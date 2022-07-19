577ed02ddd475f52233ca38dd42edb757728981c
dhruv-203 <pateldhruv2460@gmail.com>
2022-07-19 09:20:06 +0530

    added method overriding

---
+++ oops/MethodOverriding.java
@@ -0,0 +1,34 @@
+package oops;
+class A{
+    int a=45;
+    void method(){
+        System.out.println("hello world");
+    }
+    void method1(){
+        System.out.println("a+b="+45);
+    }
+
+}
+class B extends A{
+    int a = 87; // this is attribute overriding 
+    void method(int A){
+        System.out.println("a="+a);
+        System.out.println(A+" value of A");
+    }  // this is not overriding as parent class method doesn't have any parameter
+    void method1(){
+        System.out.println("a+b="+34);
+    }  // this is overriding
+}
+public class MethodOverriding {
+    public static void main(String[] args) {
+     A obja= new A();
+     B objb = new B();
+     objb.method();
+     objb.method(78);
+     obja.method1();
+     objb.method1();
+
+     System.out.println("\n\n a value in A class"+obja.a);
+     System.out.println("\n\n a value in B class"+objb.a);
+    }
+}
