9ec671c3a7bfb29c4a9fad95827b2228c1976e7b
dhruv-203 <pateldhruv2460@gmail.com>
2022-07-20 12:46:09 +0530

    half code remaining

---
+++ test2.php
@@ -0,0 +1,11 @@
+<?php
+$pudding=array('raspberry'=>333,'peach'=>222);
+$icecream=array('pecan'=>111,'chocolate'=>999);
+print_r($pudding);
+print_r($icecream);
+$dessert=$pudding+$icecream;
+print($dessert);
+// == same elements; === same element at same place 
+
+
+?>
