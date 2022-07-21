e8e5b7f163f233a56ea6562efefb5e1a733821df
dhruv-203 <pateldhruv2460@gmail.com>
2022-07-21 11:41:39 +0530

    added Intent app

---
+++ Intent/app/src/main/java/com/example/intent/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button2;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent implicit = new Intent(Intent.ACTION_VIEW);
+
+                implicit.setData(Uri.parse("https://google.com"));
+                startActivity(implicit);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent implicit = new Intent(MainActivity.this, MainActivity2.class ).putExtra("name", "username");
+                startActivity(implicit);
+            }
+        });
+
+
+    }
+}
