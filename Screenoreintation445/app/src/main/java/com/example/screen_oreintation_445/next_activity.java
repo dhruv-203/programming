2822149f9f2cc8a871fab7efe049389c291536e9
dhruv-203 <pateldhruv2460@gmail.com>
2022-08-19 16:10:54 +0530

    added screen-orientation

---
+++ Screenoreintation445/app/src/main/java/com/example/screen_oreintation_445/next_activity.java
@@ -0,0 +1,25 @@
+package com.example.screen_oreintation_445;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class next_activity extends AppCompatActivity {
+Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+        button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(next_activity.this,MainActivity.class);
+                startActivity(in);
+            }
+        });
+    }
+}
