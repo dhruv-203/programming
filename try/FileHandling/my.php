74270bba8ea5de3772670f8c6fed7c787150ffbd
dhruv-203 <pateldhruv2460@gmail.com>
2022-12-01 10:19:43 +0530

    created a folder to practice

---
+++ try/FileHandling/my.php
@@ -0,0 +1,10 @@
+<?php
+//opening a file 
+if($h = fopen("test.txt","w+")){
+    fwrite($h,"lashflashfiafioqhefojlckn.cz.czncalkfeihfilclzmcz czlknklchilfheio");
+}
+else{
+    echo "lol!!";
+}
+
+?>
